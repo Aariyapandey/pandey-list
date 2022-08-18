9157b7247a4e75d6082e60f95aa5acdd754d0803
Aariyapandey <110876834+Aariyapandey@users.noreply.github.com>
2022-08-18 12:16:22 +0530

    Add files via upload

---
+++ questions from w3schools.com.py
@@ -0,0 +1,12 @@
+# thislist = ["apple", "banana", "cherry"]
+# print(len(thislist))
+
+
+# thislist = ["apple", "banana", "cherry", "apple", "cherry"]
+# print(thislist)
+
+
+
+# mylist = ["apple", "banana", "cherry"]
+# print(type(mylist))
+
